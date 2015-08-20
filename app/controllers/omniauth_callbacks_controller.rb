3db7fdc49d2606004e4d249754ff369f37cf43b2
Ilia Khudiashov <ilia.khudiashov@gmail.com>
2015-08-20 20:00:14 +0300

    fix redirects after facebook signin

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -4,8 +4,10 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
       sign_in @user
+      redirect_to root_path
     else
       session['devise.facebook_data'] = request.env['omniauth.auth']
+      redirect_to new_user_registration_url
     end
   end
 end
