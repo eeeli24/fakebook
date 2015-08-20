2075efe7cf6fc84c2b49b4ce3166cc6fc9ce8426
Ilia Khudiashov <ilia.khudiashov@gmail.com>
2015-08-20 20:11:46 +0300

    continue fighting facebook signin

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,7 +26,9 @@ class User < ActiveRecord::Base
   def self.from_omniauth(auth)
     where(auth.slice(provider: auth.provider, uid: auth.uid)).first_or_create do |user|
       user.email    = auth.info.email
-      user.password = Devise.friendly_token[0,20]
+      pass = Devise.friendly_token[0,20]
+      user.password = pass
+      user.passwod_confirmation = pass
       user.name     = auth.info.name
     end
   end
