ed717d5f192ad2baa86a8f2c41dde19a0dfd3f4a
Ilia Khudiashov <ilia.khudiashov@gmail.com>
2015-08-21 16:01:59 +0300

    allow users to edit profile without password

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -12,4 +12,8 @@ class RegistrationsController < Devise::RegistrationsController
   def after_update_path_for(resource)
     user_path(current_user)
   end
+
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
 end
